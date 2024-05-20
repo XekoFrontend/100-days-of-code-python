4f23ebd669ae085421a65c9afbf6b7017fa95e1f
XekoFrontend <haocenter@gmail.com>
2024-05-20 15:33:49 +0700

    Adding Angela' solution in the bottom comment.

--- day-3/odd-or-even.py
+++ day-3/odd-or-even.py
@@ -2,6 +2,6 @@ print("*** Welcom to Odd or Even Checker! ***")
 number = int(input("Enter your number: "))
 # IF the number can be divided by 2 with 0 remainder
 if number % 2 == 0:
-  print(f"{number} is even number.")
+  print(f"{number} is an even number.")
 else:
-  print(f"{number} is odd number.")
+  print(f"{number} is an odd number.")
