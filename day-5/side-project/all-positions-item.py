b000725cbb369289b83e32ac160867657e933536
XekoFrontend <haocenter@gmail.com>
2024-06-04 16:36:30 +0700

    Create a folder named ‘/side-project’ and submit the ‘all-positions-item.py’ file, which contains an algorithm to find all positions of an item in a list.

---
+++ day-5/side-project/all-positions-item.py
@@ -0,0 +1,8 @@
+# Tìm tất cả vị trí của Táo
+hoa_qua = ["Lê", "táo", "táo", "chuối", "táo", "xoài", "nho", "táo", "táo"]
+
+index = 0
+for fruit in hoa_qua:
+  if fruit == "táo":    
+    print(index)
+  index += 1
