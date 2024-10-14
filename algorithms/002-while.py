f474f6f6b6b5dd4f8524310c3e67d8cfdeedbd32
XekoFrontend <haocenter@gmail.com>
2024-10-14 17:55:34 +0700

    addding while loop

---
+++ algorithms/002-while.py
@@ -0,0 +1,9 @@
+password = "lovely"
+
+input_pass= input("Enter your password: ")
+
+while password != input_pass:
+  print("You password is incorrectly, please try again!")
+  input_pass= input("Enter your password: ")
+
+print("You password is correctly. Welcome! 😊")
