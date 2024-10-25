c37646c5797535d58c944f48940d98094f65286a
XekoFrontend <haocenter@gmail.com>
2024-10-25 12:49:25 +0700

    Finish step5: Complete 90% of the program.

--- day-8/my-demo-gennewpass.py
+++ day-8/my-demo-gennewpass.py
@@ -4,31 +4,37 @@
 lowercase_alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 # User's input
 password = input("Nhập mật khẩu của bạn: ").lower()
-encrypt_choice = input("Bạn muốn mã hóa hay giải mã (e/d): ").lower()
 shift = int(input("Nhập số bước: "))
+choice = input("Bạn muốn mã hóa hay giải mã (e/d): ").lower()
+checkpoint = len(lowercase_alphabet) # checkpoint = 26
 
-# process checkpoint
-checkpoint = (len(lowercase_alphabet) -1) # int = 25
-while shift > checkpoint:    
-    new_shift = shift - checkpoint
-    if new_shift < checkpoint:
-      break 
-
-# Encryption function
-def encryption_password(password, shift):
+def encryption_password(password, shift, choice):
   new_pass = ""
   for letter in password:
     if letter in lowercase_alphabet:
-      lowercase_index = int(lowercase_alphabet.index(letter)) + shift
+      if choice == "e":
+        lowercase_index = int(lowercase_alphabet.index(letter)) + shift
+      if choice == "d":
+        lowercase_index = int(lowercase_alphabet.index(letter)) - shift
+      while lowercase_index > checkpoint or lowercase_index == checkpoint:
+          lowercase_index -= checkpoint
       letter_shift = lowercase_alphabet[lowercase_index]
-      new_pass += letter_shift
+      new_pass += letter_shift      
   print(new_pass)
-# Check for encryption or decryption
-if encrypt_choice == "e":
-  encryption_password(password, shift)
-elif encrypt_choice == 'd':
-  decryption = 0 - shift
-  encryption_password(password, decryption)
+
+encryption_password(password, shift, choice)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- day-8/my-steps/3-fix-bug-outofrange.py
+++ day-8/my-steps/3-fix-bug-outofrange.py
@@ -1,36 +1,16 @@
-import string
-alphabet = list(string.ascii_lowercase)
-old_word = "abcxyz"
-shift = 37 # Need to be <= 26 (number_alphabet)
-if shift > len(alphabet):
-  while shift > 26 :    
-    shift -= len(alphabet)
-    if shift <= 26:
-      break    
-  print(f"while {shift}")
-else:
-  print(shift)
-
-# new_word = ""
-# number_alphabet = len(alphabet)
-
-# for letter in old_word:
-#   if letter in alphabet:
-#     shift_index = alphabet.index(letter) + shift
-#     if shift_index < number_alphabet:
-#       new_word += alphabet[shift_index]
-#     else:
-#       negative_index = shift_index - number_alphabet    
-#       new_word +=alphabet[negative_index]
-
-# print(new_word)
-
-
-
 # import string
 # alphabet = list(string.ascii_lowercase)
 # old_word = "abcxyz"
-# shift = 13 # Need to be <= 26 (number_alphabet)
+# shift = 37 # Need to be <= 26 (number_alphabet)
+# if shift > len(alphabet):
+#   while shift > 26 :    
+#     shift -= len(alphabet)
+#     if shift <= 26:
+#       break    
+#   print(f"while {shift}")
+# else:
+#   print(shift)
+
 # new_word = ""
 # number_alphabet = len(alphabet)
 
@@ -44,3 +24,23 @@ else:
 #       new_word +=alphabet[negative_index]
 
 # print(new_word)
+
+
+
+import string
+alphabet = list(string.ascii_lowercase)
+old_word = "abcxyz"
+shift = 1 # Need to be <= 26 (number_alphabet)
+new_word = ""
+number_alphabet = len(alphabet)
+
+for letter in old_word:
+  if letter in alphabet:
+    shift_index = alphabet.index(letter) + shift
+    if shift_index < number_alphabet:
+      new_word += alphabet[shift_index]
+    else:
+      negative_index = shift_index - number_alphabet    
+      new_word +=alphabet[negative_index]
+
+print(new_word)
