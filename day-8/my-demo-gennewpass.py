b2c7de9cd822a52ec15dd02f991a6c91b3787959
XekoFrontend <haocenter@gmail.com>
2024-10-25 16:11:11 +0700

    finish 95%, need to adding error notifications

--- day-8/my-demo-gennewpass.py
+++ day-8/my-demo-gennewpass.py
@@ -2,11 +2,6 @@
 # ENCRYPTION PASSWORD EXERCISE
 #################################
 lowercase_alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-# User's input
-password = input("Nhập mật khẩu của bạn: ").lower()
-shift = int(input("Nhập số bước: "))
-choice = input("Bạn muốn mã hóa hay giải mã (e/d): ").lower()
-checkpoint = len(lowercase_alphabet) # checkpoint = 26
 
 def encryption_password(password, shift, choice):
   new_pass = ""
@@ -14,23 +9,28 @@ def encryption_password(password, shift, choice):
     if letter in lowercase_alphabet:
       if choice == "e":
         lowercase_index = int(lowercase_alphabet.index(letter)) + shift
+        while lowercase_index > checkpoint or lowercase_index == checkpoint:
+          lowercase_index -= checkpoint
       if choice == "d":
         lowercase_index = int(lowercase_alphabet.index(letter)) - shift
-      while lowercase_index > checkpoint or lowercase_index == checkpoint:
-          lowercase_index -= checkpoint
+        while lowercase_index < -checkpoint:
+          lowercase_index += checkpoint
+          
       letter_shift = lowercase_alphabet[lowercase_index]
       new_pass += letter_shift      
   print(new_pass)
 
-encryption_password(password, shift, choice)
 
-
-
-
-
-
-
-
+quit_program = False
+while not quit_program:
+  password = input("Nhập mật khẩu của bạn: ").lower()
+  shift = int(input("Nhập số bước: "))
+  choice = input("Bạn muốn mã hóa hay giải mã (e/d): ").lower()
+  checkpoint = len(lowercase_alphabet) # checkpoint = 26
+  encryption_password(password, shift, choice)
+  quit_choice = input("Do you want to quit program (y/n): ")
+  if quit_choice == "y":
+    quit_program = True
 
 
 
