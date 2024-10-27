60d3b1fb7996761544f4f553ba14d1eb3b316167
XekoFrontend <haocenter@gmail.com>
2024-10-27 09:30:19 +0700

    done.

--- day-8/3-caesar-cipher.py
+++ day-8/3-caesar-cipher.py
@@ -1,9 +1,6 @@
-import string
-alphabet = list(string.ascii_lowercase)
-
-direction = input("Type 'encode' or 'decode':\n")
-text = input("Type your message:\n")
-shift = int(input("Type the shift number:\n"))
+import string, caesar_logo
+print(caesar_logo.logo) # Create ascii arts: https://www.asciiart.eu/text-to-ascii-art
+alphabet = list(string.ascii_lowercase) # Create an lowercase alphabet list from 'string' module.
 
 def caesar(original_text, shift_amount, encode_or_decode):
   output_text = ""
@@ -16,9 +13,20 @@ def caesar(original_text, shift_amount, encode_or_decode):
     if letter in alphabet:   
       shifted_position = alphabet.index(letter) + shift_amount # ex: 25 + 9 = 34      
       shifted_position %= len(alphabet)                        # ex: 34 % 25 = 9
-      output_text += alphabet[shifted_position]
-    else:
-      output_text += letter
+      output_text += alphabet[shifted_position]    
+    else: # Checking and Output non-alphabet characters.
+      output_text += letter  
   print(f"Here is {encode_or_decode}d result: {output_text}")
 
-caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+should_continue = True
+while should_continue:
+  direction = input("Type 'encode' or 'decode':\n")
+  text = input("Type your message:\n")
+  shift = int(input("Type the shift number:\n"))
+
+  caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+  
+  restart = input("Type 'yes' if you want to go again. Otherwise, type 'no':\n").lower()
+  if restart == "no":
+    print("Goodbye!🤗")
+    should_continue = False    
