3bed6167d4b722df0e208cfebc54a372d6559b6c
XekoFrontend <haocenter@gmail.com>
2024-11-11 21:24:39 +0700

    TODO: Xem lại function và phần switch câu hỏi

--- day-014-higher-lower/1-mycode.py
+++ day-014-higher-lower/1-mycode.py
@@ -3,6 +3,7 @@ print(art.logo)
 
 release_A = []
 release_B = []
+score = 0
 
 # Random question
 def question(order):
@@ -21,22 +22,34 @@ def question(order):
 
 # So sánh sô lượng phát hành
 def compare_release(choice):
-    """ Compare relase amout and show notification """
+    """ Compare release amount and show notification """
     if choice == "A" and release_A > release_B:
-        print("You're right! Current score:")
+        return 1
     elif choice == "B" and release_A < release_B:
-        print("You're right! Current score:")
+        return 1
     else:
-        print("Sorry, that's wrong.Final score: ")
+        return 0
+
 
 question_A = question(order='Compare A')
-print(question_A)
-print(art.vs)
-question_B = question(order='Compare B')
-print(question_B)
 
-choice = input("Which one sells better? Type 'A' or 'B': ").capitalize()
-compare_release(choice)
+game_over = False
+while not game_over:
+    print(question_A)
+    print(art.vs)
+    question_B = question(order='Compare B')
+    print(question_B)
+
+    choice = input("Which one sells better? Type 'A' or 'B': ").capitalize()
+    compare_result = compare_release(choice)
+    # Nếu đúng thì cộng 1 điểm và tiếp tục game, nếu sai thì thoát game.
+    if compare_result == 1:
+        score += 1
+        print(f"You're right! Current score: {score}")
+        question_A = question_B
+    else:
+        print(f"Sorry, that's wrong.Final score: {score}")
+        game_over = True
 
 
 # output = compare_release(choice)
@@ -48,4 +61,5 @@ compare_release(choice)
 # TODO:
 # bug 2 câu A - B không được giống nhau
 # Nếu đúng câu B thành A (tham khảo thuật toán sắp xếp)
-# Nếu đúng + 1 điểm, sai thì thông báo rồi hỏi có chơi tiếp không?
+# bug sau khi gán A = B thì không dùng được phép so sánh. Cần xem lại phần gán câu hỏi. Xem cả trong function
+# DONE: Nếu đúng + 1 điểm, sai thì thông báo rồi hỏi có chơi tiếp không?
