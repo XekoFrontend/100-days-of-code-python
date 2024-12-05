28d776aef7524193db70addac6f98ff37f33b15f
XekoFrontend <haocenter@gmail.com>
2024-12-05 21:44:57 +0700

    day 15

---
+++ 2-intermediate/day-015/1-mydemocode.py
@@ -0,0 +1,7 @@
+asking_user = input("What would you like? (espresso/latte/cappuccino): ")
+if asking_user == "espresso":
+  dosomething
+elif asking_user == "latte":
+  do another thing
+elif asking_user == "cappuccino":
+  do another thing
