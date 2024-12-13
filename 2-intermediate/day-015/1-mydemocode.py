6b75edea32981441c3ecfd3aa2c3f475630a348f
XekoFrontend <haocenter@gmail.com>
2024-12-13 12:51:43 +0700

    Done: Returns the total calculated from coins inserted.

--- 2-intermediate/day-015/1-mydemocode.py
+++ 2-intermediate/day-015/1-mydemocode.py
@@ -32,12 +32,23 @@ resource = {
 
 # TODO 4: Check resources sufficient?
 def is_resource_sufficient(order_ingredients):
+    """ Kiểm tra nếu nguyên liệu trong kho có đủ làm đồ uống cho khách không?"""
     for item in order_ingredients:
         if order_ingredients[item] >= resource[item]:
-            # print(MENU["cost"])
             print(f"Sorry there is not enough {item}.")
+            # We're going to return False because there is not enough resources.
+            return False
+    # If the loop completes without returning False, return True.
+    return True
 
-
+def process_coins():
+    """ Returns the total calculated from coins inserted. """
+    print("Please insert coins.")
+    total = int(input("How many quarters: ")) * 0.25
+    total += int(input("How many dimes: ")) * 0.1
+    total += int(input("How many nickles: ")) * 0.05
+    total += int(input("How many pennies: ")) * 0.01
+    return total
 
 profit = 0
 
@@ -51,12 +62,13 @@ while turn_off_machine:
     # TODO 3:  print resource
     elif choice == "report":
         for i in resource:
-            print(f"{i}: {resource[i]}")
-        print(f"Money: {profit}$")
+            print(f"{i}: {resource[i]} ml")
+        print(f"Money: {profit} $")
     # TODO 4:
     elif choice == "espresso" or choice == "latte" or choice == "cappuccino":
         drink = MENU[choice]
-        is_resource_sufficient(order_ingredients=drink["ingredients"])
+        if is_resource_sufficient(order_ingredients=drink["ingredients"]):
+            payment = process_coins()
     else:
         print("Please spell correctly: espresso/latte/cappuccino/report/off")
 
